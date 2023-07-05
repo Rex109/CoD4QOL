04ad984d1b73cdaabb3cb6ab1b1d2bb0a7f6be96
Rex109 <andrea.nalin@hotmail.it>
2023-07-05 04:44:38 +0200

    Fixed wrong new version

--- cod4qol/defines.hpp
+++ cod4qol/defines.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #define COD4QOL_NAME "CoD4QOL"
-#define COD4QOL_VERSION "1.2.0"
+#define COD4QOL_VERSION "1.1.0"
 #define COD4QOL_CONSOLE_PREFIX COD4QOL_NAME " " COD4QOL_VERSION ">"
 
 #define COD4QOL_GITHUB_URL "https://api.github.com/repos/Rex109/CoD4QOL/releases/latest"
