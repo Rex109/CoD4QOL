760be0c189b5bfd9a0291cd57fb4a31042edc55d
Rex109 <andrea.nalin@hotmail.it>
2024-03-30 01:00:09 +0100

    Added force extract on update

--- cod4qol/updater.cpp
+++ cod4qol/updater.cpp
@@ -57,6 +57,9 @@ void updater::Update()
     if(download_url.empty())
         return;
 
+    commands::qol_forceiwdextract->current.enabled = true;
+    commands::qol_forceiwdextract->latched.enabled = true;
+
     std::filesystem::create_directory("download");
 
     curl_global_init(CURL_GLOBAL_ALL);
