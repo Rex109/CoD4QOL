b36852f4bacc0852040f85df679fe00449fbe5be
Rex109 <andrea.nalin@hotmail.it>
2024-11-24 14:41:21 +0100

    Removed old bhop command update

--- cod4qol/game.cpp
+++ cod4qol/game.cpp
@@ -146,7 +146,6 @@ void game::hookedDB_LoadXZoneFromGfxConfig()
 	game::Cbuf_AddText("toggleconsoleupdate\n", 0);
 	game::Cbuf_AddText("toggleloadinginfoupdate\n", 0);
 	game::Cbuf_AddText("togglesteamauthupdate\n", 0);
-	game::Cbuf_AddText("toggleenablebhopupdate\n", 0);
 	game::Cbuf_AddText("readprotectedconfig\n", 0);
 	
 	return game::pDB_LoadXZoneFromGfxConfig();
