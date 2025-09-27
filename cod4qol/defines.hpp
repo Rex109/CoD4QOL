84ae23d00f1509f41fb7af530926d0c2c1e3a07f
Rex109 <andrea.nalin@hotmail.it>
2025-09-27 18:06:26 +0200

    Added 21.3 support

--- cod4qol/defines.hpp
+++ cod4qol/defines.hpp
@@ -5,7 +5,7 @@
 #include <vector>
 
 #define COD4QOL_NAME "CoD4QOL"
-#define COD4QOL_BASE_VERSION "1.8.0"
+#define COD4QOL_BASE_VERSION "1.9.0"
 #ifndef _DEBUG
 #define COD4QOL_VERSION COD4QOL_BASE_VERSION " (" GIT_COMMIT_HASH ")"
 #else
@@ -18,11 +18,12 @@
 
 #define COD4QOL_IWD_CRC32 "fb921b14"
 
-#define COD4QOL_SUPPORTEDVERSIONS "21.1, 21.2"
+#define COD4QOL_SUPPORTEDVERSIONS "21.1, 21.2, 21.3"
 
 #define COD4QOL_COD4X_MODULE "cod4x_021.dll"
 
 #define COD4QOL_COD4X_CRC32_212 "5b855796"
 #define COD4QOL_COD4X_CRC32_211 "b1822ed5"
+#define COD4QOL_COD4X_CRC32_213 "ee0fba9d"
 
-inline const std::vector<std::string> supported_cod4x_crc32 = { COD4QOL_COD4X_CRC32_212, COD4QOL_COD4X_CRC32_211 };
+inline const std::vector<std::string> supported_cod4x_crc32 = { COD4QOL_COD4X_CRC32_213, COD4QOL_COD4X_CRC32_212, COD4QOL_COD4X_CRC32_211 };
