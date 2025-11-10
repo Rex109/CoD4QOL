9c3298914fa82cf604b736efb0ab663e767d6d20
Rex109 <andrea.nalin@hotmail.it>
2025-11-10 21:42:16 +0100

    Changed version to 1.9.2

--- cod4qol/defines.hpp
+++ cod4qol/defines.hpp
@@ -5,7 +5,7 @@
 #include <vector>
 
 #define COD4QOL_NAME "CoD4QOL"
-#define COD4QOL_BASE_VERSION "1.10.0"
+#define COD4QOL_BASE_VERSION "1.9.2"
 #ifndef _DEBUG
 #define COD4QOL_VERSION COD4QOL_BASE_VERSION " (" GIT_COMMIT_HASH ")"
 #else
